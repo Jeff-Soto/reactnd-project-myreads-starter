5d9baf36c0638f2949c145d5faf4380d13730a4b
Jeff Soto <jeffrey.soto4@gmail.com>
2017-07-19 08:20:03 -0400

    refactor: Add thumbnail/author check logic

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -22,7 +22,8 @@ class BookShelf extends Component {
             <li key={book.title}>
               <div className="book">
                 <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+                  {book.imageLinks ? <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+                : <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(https://books.google.com/googlebooks/images/no_cover_thumb.gif)`}}></div>}
                   <div className="book-shelf-changer">
                     <select value={book.shelf} onChange={(e) => this.handleUpdate({id: book.id}, e.target.value)}>
                       <option value="none" disabled>Move to...</option>
@@ -34,7 +35,7 @@ class BookShelf extends Component {
                   </div>
                 </div>
                 <div className="book-title">{book.title}</div>
-                <div className="book-authors">{book.authors[0]}</div>
+                {book.authors ? <div className="book-authors">{book.authors}</div> : <div className="book-authors">N/A </div>}
               </div>
             </li>
           )}
